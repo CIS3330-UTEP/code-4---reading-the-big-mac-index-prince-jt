4ed7c9edfabb0d8c07ba5e3d60ece3baf4acebfe
prince-jt <joe_terry3louis@yahoo.com>
2024-09-26 16:11:48 -0600

    I think the first one is fully completed, used help from chat GPT to format the structure of the f string. Will continue work on the first and second one

--- code_4.py
+++ code_4.py
@@ -2,24 +2,24 @@ import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 
-
+df = pd.read_csv('./big-mac-full-index.csv')
 
 #print(df)
 #First 2 are queries
 def get_big_mac_price_by_year(year,country_code):
-    #date format: yyyy-mm-dd
-    year = 2018
-    df = pd.read_csv('./big-mac-full-index.csv')
-    query_text = f"((date >= '{year}-01-01' and date <= '{year}-12-31
+     #date format: yyyy-mm-dd
+    query_text = f"(date >= '{year}-01-01' and date <= '{year}-12-31 and iso_a3 == '{country_code.upper()}')"
     sub_df = df.query(query_text)
-    print(sub_df)
+    mean_price = (round(sub_df['dollar_price'].mean(),2))
+    return mean_price
     
     
 
 def get_big_mac_price_by_country(country_code):
-    print(df['dollar_price'])
-    query_2 = f"(iso_a3 == df['dollar_price'])
-    c_price = df.query(query)
+    pass
+    #print(df['dollar_price'])
+    #query_2 = f"(iso_a3 == df['dollar_price'])
+    #c_price = df.query(query)
 
 #These two require indexes
 def get_the_cheapest_big_mac_price_by_year(year):
