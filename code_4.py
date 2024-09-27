b03653242e7950b3b65154736a7b2b513969423e
prince-jt <joe_terry3louis@yahoo.com>
2024-09-27 14:28:10 -0600

    I had misspelled loc on line 43, but as of now the code is giving me proper results

--- code_4.py
+++ code_4.py
@@ -40,14 +40,14 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     year_result_2 = df.query(query_4)
     max_query = year_result_2['dollar_price'].idxmax()
     expensive = round(year_result_2.loc[max_query]['dollar_price'],2)
-    row = year_result_2.low[max_query]
+    row = year_result_2.loc[max_query]
     country = row['name']
     iso_a3 = row['iso_a3']
     sentence_expensive = f"{country}({iso_a3}): ${expensive}"
     return sentence_expensive
 
 if __name__ == "__main__":
-    year = 2008
+    year = 2003
     country_code = 'MYS'
     result_1 =get_big_mac_price_by_year(year,country_code)
     print(f"The price of a bigmac in {year}, location: {country_code} was ${result_1}.")
@@ -57,3 +57,7 @@ if __name__ == "__main__":
 
     result_3 = get_the_cheapest_big_mac_price_by_year(year)
     print(f"The cheapest bigmac in {year} was in {result_3}")
+
+    result_4 = get_the_most_expensive_big_mac_price_by_year(year)
+    print(f"The most expensive big mac in {year} was in {result_4}")
+
