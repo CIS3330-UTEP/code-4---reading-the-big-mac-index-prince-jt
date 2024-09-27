2de099b76b744fa4a2f8daa3fda740210b943d78
prince-jt <joe_terry3louis@yahoo.com>
2024-09-27 14:09:43 -0600

    Code wasnt running cause of a missing single quotation in line 11, but now print statements are running

--- code_4.py
+++ code_4.py
@@ -1,4 +1,4 @@
-import csv
+#import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 
@@ -8,7 +8,7 @@ df = pd.read_csv('./big-mac-full-index.csv')
 #First 2 are queries
 def get_big_mac_price_by_year(year,country_code):
      #date format: yyyy-mm-dd
-    query_text = f"(date >= '{year}-01-01' and date <= '{year}-12-31 and iso_a3 == '{country_code.upper()}')"
+    query_text = f"(date >= '{year}-01-01' and date <= '{year}-12-31' and iso_a3 == '{country_code.upper()}')"
     sub_df = df.query(query_text)
     mean_price = (round(sub_df['dollar_price'].mean(),2))
     return mean_price
@@ -36,7 +36,21 @@ def get_the_cheapest_big_mac_price_by_year(year):
     return sentence
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    query_4 = f"(date>= '{year}-01-01' and )"
+    query_4 = f"(date >= '{year}-01-01' and date <= '{year}-12-31' )"
+    year_result_2 = df.query(query_4)
+    max_query = year_result_2['dollar_price'].idxmax()
+    expensive = round(year_result_2.loc[max_query]['dollar_price'],2)
+    row = year_result_2.low[max_query]
+    country = row['name']
+    iso_a3 = row['iso_a3']
+    sentence_expensive = f"{country}({iso_a3}): ${expensive}"
+    return sentence_expensive
 
 if __name__ == "__main__":
-    pass
+    year = 2008
+    country_code = 'MYS'
+    result_1 =get_big_mac_price_by_year(year,country_code)
+    print(f"The price of a bigmac in {year}, location: {country_code} was ${result_1}.")
+
+    result_2 = get_big_mac_price_by_country(country_code)
+    print(f"The mean price of a bigmac was currently ${result_2}")
